6602a3366d49d6f5cbe1578cc79f58ca192380ac
Charles Craig <ccraig13@gmail.com>
2013-06-01 18:30:50 -0400

    can now post and save form

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,7 @@
 'use strict';
 
-angular.module('openform')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+//angular.module('openform')
+openFormApp.controller('MainCtrl', ['$scope', 'oFormService', function ($scope, oFormService) {
+    oFormService.run();
+    oFormService.save();
+  }]);
